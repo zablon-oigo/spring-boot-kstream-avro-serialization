1c225bf324db8617d03a75939a09c269615b61cc
zablon <zablonoigo51@gmail.com>
2026-04-17 18:41:51 +0300

    app config

---
+++ src/main/java/mart/MartApplication.java
@@ -0,0 +1,13 @@
+package mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartApplication.class, args);
+	}
+
+}
